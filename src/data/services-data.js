7de1fdbd6e554cc26ff097adcb1a888e70e9b8a7
Golden0708 <chetan@goldenmileinc.com>
2025-02-07 11:11:17 -0800

    Update services-data.js

--- src/data/services-data.js
+++ src/data/services-data.js
@@ -7,13 +7,13 @@ const servicesData = [
       "With FTL, your freight is exclusively transported on a dedicated vehicle, ensuring it remains secure and separate from other products throughout the entire journey.",
     fragment: "fulltruckloadfreight",
   },
-  {
-    title: "Less Than Truckload",
-    icon: "fa fa-trailer",
-    fragment: "lessthantruckload",
-    description:
-      "Our trucking offers and cost-effective shipping solutions are tailored to small businesses, ideal for shipments ranging from one to six pallets, meeting all your business needs efficiently."
-  },
+  // {
+  //   title: "Less Than Truckload",
+  //   icon: "fa fa-trailer",
+  //   fragment: "lessthantruckload",
+  //   description:
+  //     "Our trucking offers and cost-effective shipping solutions are tailored to small businesses, ideal for shipments ranging from one to six pallets, meeting all your business needs efficiently."
+  // },
   {
     icon: "fa fa-road ",
     title: "Dedicated",
@@ -28,13 +28,13 @@ const servicesData = [
     description:
       "Our commitment to safety and reliability guarantees and ensures that your dry van freight will be shipped and delivered in perfect condition."
   },
-  {
-    title: "Intermodal",
-    icon: "fa fa-train ",
-    fragment: "intermodal",
-    description:
-      "We offer intermodal solutions that enhances your options, reducing transit times with dynamic solutions and top-notch railroad services. We prioritize speed, cost-effectiveness, and optimal transportation routes.",
-  },
+  // {
+  //   title: "Intermodal",
+  //   icon: "fa fa-train ",
+  //   fragment: "intermodal",
+  //   description:
+  //     "We offer intermodal solutions that enhances your options, reducing transit times with dynamic solutions and top-notch railroad services. We prioritize speed, cost-effectiveness, and optimal transportation routes.",
+  // },
   {
     icon: "fa fa-lightbulb",
     title: "Innovation",
@@ -50,21 +50,21 @@ const servicesData = [
       "Golden Mile's guaranteed truckload services are designed to ensure your freight shipment arrives within the selected timeframe, meeting deadlines and guaranteeing reliability."
   },
 
-  {
-    icon: "flaticon-frontal-truck",
-    title: "Consolidation",
-    fragment: "consolidation",
-    description:
-      "Enhance profitability, adhere to delivery schedules, and reduce costs with our freight consolidation service, which allows you to share the expense of a load across multiple shipments."
-  },
+  // {
+  //   icon: "flaticon-frontal-truck",
+  //   title: "Consolidation",
+  //   fragment: "consolidation",
+  //   description:
+  //     "Enhance profitability, adhere to delivery schedules, and reduce costs with our freight consolidation service, which allows you to share the expense of a load across multiple shipments."
+  // },
 
-  {
-    icon: "fa fa-truck",
-    title: "Towing",
-    fragment: "Towing",
-    description:
-      "We pairs your towing needs with the appropriate towing vehicle, ensuring you have the right solution for your specific situation."
-  },
+  // {
+  //   icon: "fa fa-truck",
+  //   title: "Towing",
+  //   fragment: "Towing",
+  //   description:
+  //     "We pairs your towing needs with the appropriate towing vehicle, ensuring you have the right solution for your specific situation."
+  // },
   {
     icon: "fa fa-building",
     title: "Warehouse Management",
@@ -72,13 +72,13 @@ const servicesData = [
     description:
       "We provide Warehouse Management services including storage, logistics, inventory control, and delivery, ensuring efficient handling and distribution of goods."
   },
-  {
-    icon: "fa fa-cubes",
-    title: "Distribution Center",
-    fragment: "distribution",
-    description:
-      "We operate a warehouse storing customer goods and products, handling cargo and inventories, and distributing them to end customers."
-  },
+  // {
+  //   icon: "fa fa-cubes",
+  //   title: "Distribution Center",
+  //   fragment: "distribution",
+  //   description:
+  //     "We operate a warehouse storing customer goods and products, handling cargo and inventories, and distributing them to end customers."
+  // },
 
 ]
 export default servicesData;

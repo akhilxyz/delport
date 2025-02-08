07659675316e04fadd0ced6b24e11447d136a661
Golden0708 <chetan@goldenmileinc.com>
2025-02-07 18:15:50 -0800

    Update bannerNew.jsx

--- src/components/home/bannerNew.jsx
+++ src/components/home/bannerNew.jsx
@@ -14,129 +14,66 @@ const LandingPage = () => {
     return () => clearInterval(interval);
   }, []);
 
+  // Centralized Text Styles
+  const textStyles = {
+    color: "white",
+    textTransform: "none", // Ensures all text is in lowercase/natural case
+  };
+
+  const headingStyles = {
+    ...textStyles,
+    fontSize: "4.8rem",
+    lineHeight: "1.2",
+    fontWeight: "650",
+    marginBottom: "20px",
+  };
+
+  const buttonStyles = {
+    padding: "15px 30px",
+    fontSize: "1rem",
+    backgroundColor: "#333",
+    color: "#d8a956",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontWeight: "600",
+  };
+
   return (
     <div
       className="landing-page"
       style={{
-        padding: "80px 40px",
+        padding: "80px 60px",
         margin: "0 auto",
         textAlign: "left",
         maxWidth: "1200px",
         position: "relative",
       }}
     >
-      <div
-        className="content"
-        style={{
-          marginBottom: "50px",
-          color: "#fff",
-        }}
-      >
-        <p
-          style={{
-              marginBottom: "15px",
-              fontSize: "1.25rem",
-              fontWeight: "450",
-              letterSpacing: "1px",
-              textTransform: "none", // Changed from "uppercase" to "none"
-              color: "white",
-              }}
-        >
+      <div className="content" style={{ marginBottom: "50px", color: "#fff" }}>
+        <p style={{ ...textStyles, marginBottom: "15px", fontSize: "1.25rem", fontWeight: "450", letterSpacing: "1px" }}>
           Welcome to{" "}
-        <span
-           className="highlight"
-           style={{
-                   color: "#e1aa4c",
-                   fontWeight: "700",
-                   textTransform: "none", // Ensures "Golden Mile" stays lowercase
-                  }}
-        >
-         Golden Mile
-        </span>
+          <span className="highlight" style={{ color: "#e1aa4c", fontWeight: "700" }}>
+            Golden Mile
+          </span>
         </p>
 
-        <p
-          className="fade-text"
-          style={{
-            fontSize: "4.8rem",
-            lineHeight: "1.2",
-            fontWeight: "650",
-            marginBottom: "20px",
-          }}
-        >
-          {bannerData[currentBanner]?.banner_title || "Setting The Gold"}
-          <br />
+        <p className="fade-text" style={headingStyles}>
+          {bannerData[currentBanner]?.banner_title || "Setting The Gold"} <br />
           {bannerData[currentBanner]?.title_two || "Standard In Transportation"}
         </p>
-        <div
-          className="buttons"
-          style={{
-            marginTop: "20px",
-          }}
-        >
-          <button
-            className="btn"
-            style={{
-              padding: "15px 30px",
-              marginRight: "15px",
-              fontSize: "1rem",
-              backgroundColor: "#333",
-              color: "#d8a956",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer",
-              fontWeight: "600",
-            }}
-          >
-            GET PRICING
-          </button>
-          <button
-            className="btn"
-            style={{
-              padding: "15px 30px",
-              fontSize: "1rem",
-              backgroundColor: "#333",
-              color: "#d8a956",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer",
-              fontWeight: "600",
-            }}
-          >
-            OUR SERVICES
-          </button>
+
+        <div className="buttons" style={{ marginTop: "20px" }}>
+          <button className="btn" style={{ ...buttonStyles, marginRight: "15px" }}>GET PRICING</button>
+          <button className="btn" style={buttonStyles}>OUR SERVICES</button>
         </div>
       </div>
-      <div
-        className="arrows"
-        style={{
-          marginTop: "20px",
-          position: "absolute",
-          bottom: "20px",
-          right: "20px",
-        }}
-      >
-        <div
-          className="arrow left"
-          style={{
-            display: "inline-block",
-            marginRight: "15px",
-            cursor: "pointer",
-            fontSize: "1.5rem",
-            color: "#d8a956",
-          }}
-        >
+
+      <div className="arrows" style={{ marginTop: "20px", position: "absolute", bottom: "20px", right: "20px" }}>
+        <div className="arrow left" style={{ display: "inline-block", marginRight: "15px", cursor: "pointer", fontSize: "1.5rem", color: "#d8a956" }}>
           &lt;
         </div>
-        <div
-          className="arrow right"
-          style={{
-            display: "inline-block",
-            cursor: "pointer",
-            fontSize: "1.5rem",
-            color: "#d8a956",
-          }}
-        >
+        <div className="arrow right" style={{ display: "inline-block", cursor: "pointer", fontSize: "1.5rem", color: "#d8a956" }}>
           &gt;
         </div>
       </div>
